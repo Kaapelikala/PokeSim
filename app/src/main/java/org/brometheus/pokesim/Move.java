b796062d2fe4430250c9390b9374a636aa772314
kaapelikala <lordflak@gmail.com>
2015-01-14 11:04:20 +0200

    eka

---
+++ app/src/main/java/org/brometheus/pokesim/Move.java
@@ -0,0 +1,7 @@
+package org.brometheus.pokesim;
+
+/**
+ * Created by Kalle on 14.1.2015.
+ */
+public class Move {
+}
---
+++ app/src/main/java/org/brometheus/pokesim/Pokemon.java
@@ -0,0 +1,17 @@
+package org.brometheus.pokesim;
+
+/**
+ * Created by Kalle on 14.1.2015.
+ */
+public class Pokemon {
+
+    private int species;
+    private char[] ivs;
+    private char[] evs;
+    private int[] moves;
+    private char[] status;
+
+    
+
+
+}
